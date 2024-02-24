28bce641adff593a7657f14de75b27c043d20507
apandey007 <ap9647595935@gmail.com>
2024-02-24 15:24:40 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h1',{},'Hello world from react');
+const root = ReactDOM.createRoot(document.querySelector('#root'));              //Root is Top most DOM file
+root.render(heading);
---
+++ README.md
@@ -0,0 +1,1 @@
+#This is my Namaste React Code
